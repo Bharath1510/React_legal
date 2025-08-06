0aa5971654ad4b800b9f5fca3111950a1f7a4a35
Bharath Kumar Kathula <bharath15101998@gmail.com>
2025-08-06 12:25:37 +0530

    Remove duplicate App.tsx files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+// This file is not used in SPFx projects
+// The entry point is now src/webparts/legalDashboard/LegalDashboardWebPart.ts
+console.log('SPFx project - main.tsx not used');
